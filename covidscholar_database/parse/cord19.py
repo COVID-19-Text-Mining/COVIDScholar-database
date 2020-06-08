9836e974996d6b854a67ae666c6cdfb574cc02f8
bwjustus@berkeley.edu <bwjustus@berkeley.edu>
2020-06-08 12:55:44 -0700

    edge case and correct subscripting on _parse_document_type

--- covidscholar_database/parse/cord19.py
+++ covidscholar_database/parse/cord19.py
@@ -349,12 +349,15 @@ class CORD19Parser(Parser):
     def _parse_document_type(self, doc):
         """ Returns the document type of a document as a <class 'str'>.
         e.g. 'paper', 'clinical_trial', 'patent', 'news', 'chapter'. """
+        
         doi = self._parse_doi(doc)
+        if doi is None:
+            return 'paper'
         
         works = Works()
-        doc_type = works.doi(doi)
+        doc_info = works.doi(doi)
         
-        if doc_type == 'book-chapter':
+        if doc_info['type'] == 'book-chapter':
             return 'chapter'
         else:
             return 'paper'
